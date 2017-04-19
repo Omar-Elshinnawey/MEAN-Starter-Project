1ea7633f0bec2ee3a011904d05687ecc0313d50b
Omar Elshinnawey <el-shinnaweyom@hotmail.com>
2017-04-19 19:06:19 +0300

    added hello world example

---
+++ MEAN project/public/ts/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: '/assets/views/app.component.html',
+    styleUrls: ['assets/css/app.component.css']
+})
+export class AppComponent{
+
+    title = 'Hello World'
+
+}
